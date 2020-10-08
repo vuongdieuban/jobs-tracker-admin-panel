768600dfbae95b9f92cad43b503c831e15b1aa22
ban vuong <vuongdieuban@gmail.com>
2020-10-08 12:35:14 -0600

    abble to connect to socket.io server

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,15 @@ import React, { useEffect, useState } from 'react';
 import { Route, Switch, useHistory } from 'react-router-dom';
 import { JobApplicationManagement } from './pages/job-applications-management/JobApplicationManagement';
 import { Login } from './pages/login/Login';
+import { connect } from 'socket.io-client';
+
+const socket = connect('ws://api.jobs-tracker.localhost');
+
+socket.on('connection', (data: any) => console.log('connected', data));
+
+socket.on('msgToClient', (data: any) => console.log('rcv from Server:', data));
+
+socket.emit('msgToServer');
 
 function App() {
   const history = useHistory();
